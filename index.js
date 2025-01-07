4494d3def8cae4a74e5c85d40292c19743808807
sadman249 <mdsadman249@gmail.com>
2025-01-07 11:45:16 +0600

    added the userAdmin verefy

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ app.use(cors());
 app.use(express.json());
 
 
-// const uri = `${process.env.MODB_URL}`;
+const uri = `${process.env.MODB_URL}`;
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ttzwe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ttzwe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
